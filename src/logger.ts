bfad00d60add799ef8c2153a785835af2a3fc86b
Kevin Dean <Kevin.Dean@datadevelopment.com>
2025-12-23 08:16:10 -0300

    Updated logging to expose all tslog capabilities.

--- src/logger.ts
+++ src/logger.ts
@@ -1,4 +1,4 @@
-import { Logger } from "tslog";
+import { type ISettingsParam, Logger } from "tslog";
 
 /**
  * Log levels.
@@ -24,29 +24,62 @@ export type LogLevelKey = keyof typeof LogLevels;
 export type LogLevel = typeof LogLevels[LogLevelKey];
 
 /**
- * Get a simple logger with an optional log level.
+ * Get the log level enumeration value corresponding to a string or number.
+ *
+ * @param untypedLogLevel
+ * Untyped log level.
+ *
+ * @returns
+ * Typed log level or default `LogLevels.Info` if untyped log level not provided..
+ */
+export function logLevelOf(untypedLogLevel?: string | number): LogLevel {
+    let typedLogLevel: LogLevel;
+
+    if (typeof untypedLogLevel === "string") {
+        if (untypedLogLevel in LogLevels) {
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-type-assertion -- String exists as a key.
+            typedLogLevel = LogLevels[untypedLogLevel as LogLevelKey];
+        } else {
+            throw new Error(`Unknown log level ${untypedLogLevel}`);
+        }
+    } else if (untypedLogLevel !== undefined) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-type-assertion -- Assume that valid log level has been provided.
+        if (Object.values(LogLevels).includes(untypedLogLevel as LogLevel)) {
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-type-assertion -- Valid log level has been provided.
+            typedLogLevel = untypedLogLevel as LogLevel;
+        } else {
+            throw new Error(`Unknown log level ${untypedLogLevel}`);
+        }
+    } else {
+        typedLogLevel = LogLevels.Info;
+    }
+
+    return typedLogLevel;
+}
+
+/**
+ * Get a logger with an optional log level. The underlying implementation is
+ * [`tslog`](https://tslog.js.org/).
  *
  * @param logLevel
- * Log level as enumeration value or string.
+ * Log level as enumeration value or string. Mapped to `minLevel` in settings.
+ *
+ * @param settings
+ * Detailed settings. See [`tslog`](https://tslog.js.org/#/?id=settings) documentation for details.
+ *
+ * @param logObj
+ * Default log object. See [`tslog`](https://tslog.js.org/#/?id=defining-and-accessing-logobj) documentation for
+ * details.
  *
  * @returns
  * Logger.
+ *
+ * @template T
+ * Log object type.
  */
-export function getLogger(logLevel?: string | number): Logger<unknown> {
-    let minLevel: number;
-
-    if (typeof logLevel === "string") {
-        if (logLevel in LogLevels) {
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-type-assertion -- String exists as a key.
-            minLevel = LogLevels[logLevel as LogLevelKey];
-        } else {
-            throw new Error(`Unknown log level ${logLevel}`);
-        }
-    } else {
-        minLevel = logLevel ?? LogLevels.Info;
-    }
-
+export function getLogger<T extends object = object>(logLevel?: string | number, settings?: ISettingsParam<T>, logObj?: T): Logger<T> {
     return new Logger({
-        minLevel
-    });
+        ...settings ?? {},
+        minLevel: logLevelOf(logLevel)
+    }, logObj);
 }
