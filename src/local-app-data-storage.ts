00571d07dbd04b8ee7b25ba432bb55c20244b0fe
Kevin Dean <Kevin.Dean@datadevelopment.com>
2026-01-12 17:01:03 -0500

    Updated documentation.

--- src/local-app-data-storage.ts
+++ src/local-app-data-storage.ts
@@ -1,12 +1,9 @@
 import type { AppDataStorage } from "./app-data-storage.js";
 
 /**
- * Get the local application data storage class for the current environment. The function is asynchronous as the
+ * Local application data storage class for the current environment. This is a variable representing a `Promise` as the
  * implementing class is loaded dynamically to prevent the inclusion of unnecessary node dependencies in a browser
  * environment.
- *
- * @returns
- * Local application data storage class.
  */
 export const LocalAppDataStorage: Promise<new (path?: string) => AppDataStorage<boolean>> =
     // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition -- localStorage is undefined when running under Node.js.
