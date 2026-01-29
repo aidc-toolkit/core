72c94361deb25438b9f3e6a9c7b0731ec0c31516
Kevin Dean <Kevin.Dean@datadevelopment.com>
2026-01-29 12:16:26 -0500

    Added polyfill detection.

--- src/file-app-data-storage.ts
+++ src/file-app-data-storage.ts
@@ -7,6 +7,11 @@ import { AppDataStorage } from "./app-data-storage.js";
  */
 export class FileAppDataStorage extends AppDataStorage<true> {
     /**
+     * If true, node:fs has been polyfilled with empty object.
+     */
+    readonly #isNoOp: boolean;
+
+    /**
      * Constructor.
      *
      * @param path
@@ -14,36 +19,45 @@ export class FileAppDataStorage extends AppDataStorage<true> {
      */
     constructor(path?: string) {
         super(true, path);
+
+        // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition -- Condition is true in polyfilled environment.
+        this.#isNoOp = fs?.promises?.readFile === undefined;
     }
 
     /**
      * @inheritDoc
      */
     protected override async doRead(key: string, asBinary: boolean | undefined): Promise<string | Uint8Array | undefined> {
-        return fs.promises.readFile(key).then(buffer =>
-            asBinary === true ? buffer : buffer.toString()
-        ).catch(() =>
-            undefined
-        );
+        return !this.#isNoOp ?
+            fs.promises.readFile(key).then(buffer =>
+                asBinary === true ? buffer : buffer.toString()
+            ).catch(() =>
+                undefined
+            ) :
+            undefined;
     }
 
     /**
      * @inheritDoc
      */
     protected override async doWrite(key: string, data: string | Uint8Array): Promise<void> {
-        return fs.promises.mkdir(path.dirname(key), {
-            recursive: true
-        }).then(async () =>
-            fs.promises.writeFile(key, data)
-        );
+        return !this.#isNoOp ?
+            fs.promises.mkdir(path.dirname(key), {
+                recursive: true
+            }).then(async () =>
+                fs.promises.writeFile(key, data)
+            ) :
+            undefined;
     }
 
     /**
      * @inheritDoc
      */
     protected override async doDelete(key: string): Promise<void> {
-        return fs.promises.rm(key, {
-            force: true
-        });
+        return !this.#isNoOp ?
+            fs.promises.rm(key, {
+                force: true
+            }) :
+            undefined;
     }
 }
