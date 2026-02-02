9747cd8c3bc8b3c44f79331a66278b3ccc26d52d
Kevin Dean <Kevin.Dean@datadevelopment.com>
2026-02-02 10:43:33 -0500

    Restored non-breaking space to French guillemets.

--- src/locale/fr/locale-resources.ts
+++ src/locale/fr/locale-resources.ts
@@ -1,6 +1,6 @@
 export default {
     Logger: {
-        unknownLogLevel: "Niveau de journalisation inconnu «{{logLevel}}»"
+        unknownLogLevel: "Niveau de journalisation inconnu « {{logLevel}} »"
     },
     RemoteAppDataStorage: {
         httpError: "Erreur HTTP {{status, number}} lors de la récupération du fichier"
